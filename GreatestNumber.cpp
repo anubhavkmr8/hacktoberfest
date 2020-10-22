f8fcf8c3c355495b6646963ec8a8e21f11d9f8c7
Aditi <60514137+aditi1226@users.noreply.github.com>
2020-10-22 16:31:16 +0530

    Update GreatestNumber.cpp

--- GreatestNumber.cpp
+++ GreatestNumber.cpp
@@ -8,8 +8,8 @@ int main() {
     ll n,i,j;
     n=10;
     ll a[]={1,2,3,100,5,6,7,8,9,10};
-    ll mx=INT_MAX;  
-    for(i=1;i<=10;i++)
+    ll mx=a[0];  
+    for(i=0;i<10;i++)
     {
         mx=max(mx,a[i]);
     }
