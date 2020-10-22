801b195afd49483b2aeb7176d5adacb2449f99cd
Aditi <60514137+aditi1226@users.noreply.github.com>
2020-10-22 16:41:02 +0530

    Update SwapTwoNumbers.cpp

--- SwapTwoNumbers.cpp
+++ SwapTwoNumbers.cpp
@@ -1,5 +1,11 @@
 // C++ program to swap two variables 
-#include <stdio.h> 
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define se second
+using namespace std;
 
 int main() 
 { 
